4a073bea7539a7bce810fbf31eb1aa57de20d678
James Wilkinson <jameserwilk@gmail.com>
2025-07-24 20:22:39 -0400

    Update data

--- src/App.tsx
+++ src/App.tsx
@@ -632,6 +632,14 @@ export default function App() {
               </div>
             </section>
           </main>
+          <footer className="bg-gray-100">
+            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+              <p className="text-center text-sm text-gray-500">
+                Data &copy; 2025 TIFF. Not affiliated with TIFF. This is a fan
+                project.
+              </p>
+            </div>
+          </footer>
         </div>
       </div>
     </DndContext>
